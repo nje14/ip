f14570f37b36e278b286dce6220c8f4297a55ed0
NJE <h1710101@gmail.com>
2026-09-01 21:05:16 +0800

    Add basic launcher class

---
+++ src/main/java/nyonbot/Main.java
@@ -0,0 +1,22 @@
+package nyonbot;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private NyonBot nyonBot = new NyonBot();
+
+    @Override
+    public void start(Stage stage) {
+       
+    }
+}
---
+++ src/main/java/nyonbot/MainWindow.java
@@ -0,0 +1,15 @@
+package nyonbot;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ScrollPane;
+
+
+/**
+ * Controller class for the main GUI
+ */
+public class MainWindow extends AnchorPane{
+    @FXML
+    private ScrollPane ScrollPane;
+    
+}
